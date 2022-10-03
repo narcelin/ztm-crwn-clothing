b8b96a4badb2e86868be96dcb782faa3feb9b5dd
NicolasArcelin <103464938+NicolasArcelin@users.noreply.github.com>
2022-10-03 15:17:48 -0400

    + functional cartDropdown

--- src/routes/cart-icon/cart-icon.component.jsx
+++ src/routes/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as ShoppingIcon } from "./../../assets/shopping-bag.svg"
 import "./cart-icon.styles.scss";
 
 const CartIcon = () => {
-  const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
   const toggleIsCartOpen = () => {
     setIsCartOpen(!isCartOpen);
@@ -20,7 +20,7 @@ const CartIcon = () => {
   return (
     <div className="cart-icon-container" onClick={toggleIsCartOpen}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">10</span>
+      <span className="item-count">{cartCount}</span>
     </div>
   );
 };
